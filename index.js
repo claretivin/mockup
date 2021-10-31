d790cc41b82dd39eb2031dec1101ae2b5f8f01b3
claret_ivin <claretivin.21it@licet.ac.in>
2021-10-31 19:07:37 +0530

    ivin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    // Add smooth scrolling to all links in navbar + footer link
+    //  smooth scrolling 
     $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-      // Make sure this.hash has a value before overriding default behavior
+      
       if (this.hash !== "") {
         // Prevent default anchor click behavior
         event.preventDefault();
@@ -10,7 +10,7 @@ $(document).ready(function(){
         var hash = this.hash;
   
         // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        //(900) specifies the number of milliseconds takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 900, function(){
